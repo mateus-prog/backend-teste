a1ddacb75661ccd4ee9db0d44d0b1286b499ea76
Mateus Guizelini <dev.guizelini@hotmail.com>
2025-06-02 09:31:57 -0300

    iniciando projeto teste

---
+++ app/Http/Controllers/Controller.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Routing\Controller as BaseController;
+
+/**
+ * @OA\OpenApi(
+ *     @OA\Info(
+ *         title="Teste API",
+ *         version="1.0.0",
+ *         description="Teste LIVRO"
+ *     )
+ * )
+ *
+ * @OA\Tag(
+ *      name="Authors",
+ *      description=""
+ * )
+ * 
+ * @OA\Tag(
+ *      name="Subjects",
+ *      description=""
+ * )
+*/
+class Controller extends BaseController
+{
+    use AuthorizesRequests, ValidatesRequests;
+}
